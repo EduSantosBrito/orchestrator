9fcb521df269c3308524368a3915e2e4e362844b
EduSantosBrito <edu.santos.brito@gmail.com>
2021-06-18 10:05:12 -0300

    feat: include login project

--- src/brito-root-config.js
+++ src/brito-root-config.js
@@ -4,19 +4,29 @@ import { registerApplication, start } from "single-spa";
 //   return (location) => routes.some((route) => location.pathname === route);
 // }
 
-function showWhenPrefix(routes) {
-  return (location) =>
-    routes.some((route) => location.pathname.startsWith(route));
+// function showWhenPrefix(routes) {
+//   return (location) =>
+//     routes.some((route) => location.pathname.startsWith(route));
+// }
+
+function showExcept(routes) {
+  return (location) => routes.every((route) => location.pathname !== route);
 }
 
-// function showExcept(routes) {
-//   return (location) => routes.every((route) => location.pathname !== route);
-// }
+function showOnly(routes) {
+  return (location) => routes.every((route) => location.pathname === route);
+}
 
 registerApplication({
   name: "@brito/header",
   app: () => System.import("@brito/header"),
-  activeWhen: showWhenPrefix(["/"]),
+  activeWhen: showExcept(["/login"]),
+});
+
+registerApplication({
+  name: "@brito/login",
+  app: () => System.import("@brito/login"),
+  activeWhen: showOnly(["/login"]),
 });
 
 start({
